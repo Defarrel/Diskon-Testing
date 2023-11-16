fe3735a760d05af014a90b72c26f4749cae71ed0
defarrel <Defarrel>
2023-11-16 20:52:31 +0700

    Add project files.

---
+++ Test.Project.Library/Diskon.cs
@@ -0,0 +1,76 @@
+﻿namespace Diskon
+{
+    using System;
+
+    public class Program
+    {
+
+        public static double HitungDiskon(int kuantitasProduk)
+        {
+            if (kuantitasProduk < 20)
+                return 0.02;
+            else if (kuantitasProduk < 40)
+                return 0.04;
+            else if (kuantitasProduk < 60)
+                return 0.06;
+            else if (kuantitasProduk < 80)
+                return 0.08;
+            else if (kuantitasProduk < 100)
+                return 0.10;
+            else
+                return 0.20;
+        }
+
+        public static void TampilkanDiskon(double diskon)
+        {
+            Console.WriteLine($"Diskon yang Anda dapatkan: {diskon * 100}%");
+        }
+
+        public static void Main()
+        {
+            double hargaProduk = 0; // Inisialisasi dengan nilai default
+            bool hargaValid = false;
+
+            while (!hargaValid)
+            {
+                Console.WriteLine("Masukkan harga produk per unit: ");
+                if (double.TryParse(Console.ReadLine(), out hargaProduk))
+                {
+                    hargaValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Harga produk tidak valid. Harap masukkan angka.");
+                }
+            }
+
+            int kuantitasProduk = 0; // Inisialisamsi dengan nilai default
+            bool kuantitasValid = false;
+
+            while (!kuantitasValid)
+            {
+                Console.WriteLine("Masukkan kuantitas produk: ");
+                if (int.TryParse(Console.ReadLine(), out kuantitasProduk))
+                {
+                    kuantitasValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Kuantitas produk tidak valid. Harap masukkan angka.");
+                }
+            }
+
+            // Hitung diskon
+            double diskon = HitungDiskon(kuantitasProduk);
+
+            // Tampilkan diskon
+            TampilkanDiskon(diskon);
+
+            // Hitung total harga setelah diskon
+            double totalHarga = hargaProduk * kuantitasProduk * (1 - diskon);
+
+            Console.WriteLine($"Total Harga: {totalHarga}");
+        }
+    }
+
+}
---
+++ Test.Project.Tests/Diskon_Test.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+
+namespace Diskon.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test_HitungDiskon()
+        {
+            // Arrange
+            int quantity1 = 10;
+            int quantity2 = 30;
+            int quantity3 = 50;
+            int quantity4 = 70;
+            int quantity5 = 90;
+            int quantity6 = 110;
+
+            // Act
+            double discount1 = Program.HitungDiskon(quantity1);
+            double discount2 = Program.HitungDiskon(quantity2);
+            double discount3 = Program.HitungDiskon(quantity3);
+            double discount4 = Program.HitungDiskon(quantity4);
+            double discount5 = Program.HitungDiskon(quantity5);
+            double discount6 = Program.HitungDiskon(quantity6);
+
+            // Assert
+            Assert.AreEqual(0.02, discount1);
+            Assert.AreEqual(0.04, discount2);
+            Assert.AreEqual(0.06, discount3);
+            Assert.AreEqual(0.08, discount4);
+            Assert.AreEqual(0.10, discount5);
+            Assert.AreEqual(0.20, discount6);
+        }
+
+        [TestMethod]
+        public void Test_TampilkanDiskon()
+        {
+            // Arrange
+            double discount = 0.15;
+            string expectedOutput = "Diskon yang Anda dapatkan: 15%";
+
+            // Act
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.TampilkanDiskon(discount);
+                string actualOutput = sw.ToString().Trim();
+
+                // Assert
+                Assert.AreEqual(expectedOutput, actualOutput);
+            }
+        }
+
+    }
+}
